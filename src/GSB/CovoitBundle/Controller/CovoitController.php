43ec9e19b2fc41f1d1f9126613c047a3fbc0a8ec
Thibault Poujat <thibault.poujat@gmail.com>
2017-03-02 12:30:19 +0100

    Vue accueil
    Liste des véhicule "en dur", à modifier avec les entités

--- src/GSB/CovoitBundle/Controller/CovoitController.php
+++ src/GSB/CovoitBundle/Controller/CovoitController.php
@@ -3,11 +3,45 @@
 namespace GSB\CovoitBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CovoitController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GSBCovoitBundle:Covoit:index.html.twig');
+      // en attedant de récupérer en bdd
+      $listAnnonces = array(
+        array(
+        'id'      => 1,
+        'auteur'  => "Jean Tissipe",
+        'date_trajet'    => new \Datetime(),
+        'heure_trajet'   => new \Datetime(),
+        'ville'       => "Arpajon-sur-Cère",
+        'aller_ou_retour' => true,
+        'type_vehicule' => "4x4",
+        'commentaire'    => "Détour possible."),
+      array(
+        'id'      => 1,
+        'auteur'  => "Marc Assain",
+        'date_trajet'    => new \Datetime(),
+        'heure_trajet'   => new \Datetime(),
+        'ville'       => "Saint-Simon",
+        'aller_ou_retour' => false,
+        'type_vehicule' => "Berline",
+        'commentaire'    => "Je fais mon premier covoiturage"),
+      array(
+        'id'      => 1,
+        'auteur'  => "Abby Ciclette",
+        'date_trajet'    => new \Datetime(),
+        'heure_trajet'   => new \Datetime(),
+        'ville'       => "Giou-de-Mammou",
+        'aller_ou_retour' => true,
+        'type_vehicule' => "Vélo",
+        'commentaire'    => "Je part à l'heure !"
+      ));
+
+      return $this->render('GSBCovoitBundle:Covoit:index.html.twig',
+                              array('listAnnonces' => $listAnnonces));
     }
 }
