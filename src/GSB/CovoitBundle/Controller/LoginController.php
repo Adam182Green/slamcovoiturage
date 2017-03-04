901c10704b439726847ccf7d29e4819761f743d9
Thibault Poujat <thibault.poujat@gmail.com>
2017-03-04 23:15:53 +0100

    Modification msg flashbag

--- src/GSB/CovoitBundle/Controller/LoginController.php
+++ src/GSB/CovoitBundle/Controller/LoginController.php
@@ -40,7 +40,7 @@ class LoginController extends Controller
                 if($salarie->getEmail() == $login->getEmail() && $salarie->getMotDePasse() == $login->getMotDePasse())
                 {
                     $session->set('currentUser', $salarie);
-                    $this->addFlash('success','Identification réussie ! Bienvenue '.$salarie->getNom());
+                    $this->addFlash('success','Identification réussie ! Bienvenue '.$salarie->getPrenom() $salarie->getNom());
                     return $this->redirectToRoute('gsb_covoit_homepage');
                 }
             }
