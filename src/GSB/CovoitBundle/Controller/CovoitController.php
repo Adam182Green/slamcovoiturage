abdb18656c01e9a05144cf865a3bfcfc26130380
Thibault Poujat <thibault.poujat@gmail.com>
2017-03-05 00:23:43 +0100

    Gére layout offline et main

--- src/GSB/CovoitBundle/Controller/CovoitController.php
+++ src/GSB/CovoitBundle/Controller/CovoitController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace GSB\CovoitBundle\Controller;
-
 use GSB\CovoitBundle\Entity\Demande;
 use GSB\CovoitBundle\Entity\Salarie;
 use GSB\CovoitBundle\Entity\Trajet;
@@ -9,63 +7,104 @@ use GSB\CovoitBundle\Entity\TypeVehicule;
 use GSB\CovoitBundle\Entity\Ville;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class CovoitController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
       // On récupère l'EntityManager
       $em = $this->getDoctrine()->getManager();
-
+      $session = $request->getSession();
+      $curentUser = $session->get('currentUser');
       $listTrajets  = $em->getRepository('GSBCovoitBundle:Trajet')->findAll();
-      $listDemandes = $em->getRepository('GSBCovoitBundle:Demande')->findAll();
-
       return $this->render('GSBCovoitBundle:Covoit:index.html.twig',
                               array('listTrajets' => $listTrajets,
-                                    'listDemandes'=> $listDemandes));
+                                    'title' => 'Accueil',
+                                    'subtitle' => 'Accueil',
+                                    'currentUser' => $curentUser));
     }
-
-    public function trajetAction($id)
+    public function trajetAction(Request $request, $id)
     {
       $em = $this->getDoctrine()->getManager();
-
+      $session = $request->getSession();
+      $curentUser = $session->get('currentUser');
       $trajet = $em->getRepository('GSBCovoitBundle:Trajet')->find($id);
-
       if (null === $trajet) {
         throw new NotFoundHttpException("Le trajet d'id ".$id." n'existe pas.");
       }
 
       return $this->render('GSBCovoitBundle:Covoit:trajet.html.twig',
-                          array('trajet'  => $trajet));
+                          array('trajet'  => $trajet,
+                                'currentUser' => $curentUser));
     }
-
-    public function salarieAction($id)
+    public function salarieAction(Request $request, $id)
     {
       $em = $this->getDoctrine()->getManager();
-
+      $session = $request->getSession();
+      $curentUser = $session->get('currentUser');
       $salarie = $em->getRepository('GSBCovoitBundle:Salarie')->find($id);
-
       if (null === $salarie) {
         throw new NotFoundHttpException("Le salarie d'id ".$id." n'existe pas.");
       }
-
       return $this->render('GSBCovoitBundle:Covoit:salarie.html.twig',
-                          array('salarie'  => $salarie));
+                          array('salarie'  => $salarie,
+                                'currentUser' => $curentUser));
     }
-
-    public function menuAction($limite)
+    public function menuAction(Request $request, $limite)
     {
       if ($limite == NULL)
         $limite = 3;
-
       // On récupère l'EntityManager
       $em = $this->getDoctrine()->getManager();
-
+      $session = $request->getSession();
+      $curentUser = $session->get('currentUser');
       $listTrajets  = $em->getRepository('GSBCovoitBundle:Trajet')->findAll();
-
       return $this->render('GSBCovoitBundle:Covoit:menu.html.twig',
                           array('listTrajets' => $listTrajets,
-                                'limite'      => $limite));
+                                'limite'      => $limite,
+                                'currentUser' => $curentUser));
+    }
+
+    public function mes_trajetsAction(Request $request)
+    {
+      $em = $this->getDoctrine()->getManager();
+      $session = $request->getSession();
+      $curentUser = $session->get('currentUser');
+      $title = "Mes trajets";
+      $subtitle = "Mes trajets";
+      $trajets = $em->getRepository('GSBCovoitBundle:Trajet')->findBy(array('auteurId' => $curentUser->getId()));
+      $mes_trajets = array();
+
+      foreach ($trajets as $trajet)
+      {
+          $mes_trajets[] = $trajet;
+      }
+      if (NULL === $mes_trajets) {
+        throw new NotFoundHttpException("Vous n'avez pas encore proposé de trajet.");
+      }
+      return $this->render('GSBCovoitBundle:Covoit:index.html.twig',
+                          array('listTrajets'  => $mes_trajets,
+                                'title' => $title,
+                                'subtitle' => $subtitle,
+                                'currentUser' => $curentUser));
+    }
+
+    public function mesReservationsAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $request->getSession();
+        $curentUser = $session->get('currentUser');
+        $demandes = $em->getRepository('GSBCovoitBundle:Demande')->findBy(array('salarieId' => $curentUser->getId()));
+        $trajets = array();
+
+        foreach($demandes as $demande)
+        {
+            $trajet = $em->getRepository('GSBCovoitBundle:Trajet')->findOneById($demande->getTrajetId());
+            array_push($trajets, $trajet);
+        }
+        return $this->render('GSBCovoitBundle:Covoit:index.html.twig',
+                          array('listTrajets'  => $trajets, 'title' => 'Mes réservations', 'subtitle' => 'Mes réservations', 'currentUser' => $curentUser));
     }
 }
--- src/GSB/CovoitBundle/Controller/LoginController.php
+++ src/GSB/CovoitBundle/Controller/LoginController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace GSB\CovoitBundle\Controller;
-
 use GSB\CovoitBundle\Entity\Login;
 use GSB\CovoitBundle\Entity\Salarie;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -16,63 +14,39 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class LoginController extends Controller
 {
-
     public function loginAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $login = new Login();
         $salaries = $em->getRepository('GSBCovoitBundle:Salarie')->findAll();
         $session = $request->getSession();
-
         $form = $this->createFormBuilder($login)
             ->add('email', EmailType::class)
             ->add('motDePasse', PasswordType::class)
             ->add('connexion', SubmitType::class, array('label' => "Connexion"))
             ->getForm();
-
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
-
             $login = $form->getData();
             foreach($salaries as $salarie)
             {
                 if($salarie->getEmail() == $login->getEmail() && $salarie->getMotDePasse() == $login->getMotDePasse())
                 {
-                    // 1 - ICI AJOUTER $salarie en session (voir si on peut ajouter direct l'objet "salarie", sinon :)
-                    //$session->set('id', '$login->getId()');
-                    //$session->set('nom', '$login->getNom()');
-                    //$session->set('prenom', '$login->getPrenom()');
-                    //...
-
-                    // 2 - Ajouter message flash :
-                    //$this->addFlash('success','Identification réussie !');
-
-                    // 3 - L'afficher sur la page vers laquelle on se redirige, ici homepage
-                    /* {% if app.session.flashBag.has('success') %}
-                            <div class="alert alert-success">
-                                {% for msg in app.session.flashBag.get('success') %}
-                                    {{ msg }}
-                                {% endfor %}
-                            </div>
-                        {% endif %}*/
-
+                    $session->set('currentUser', $salarie);
+                    $this->addFlash('success','Identification réussie ! Bienvenue '.$salarie->getPrenom().' '.$salarie->getNom());
                     return $this->redirectToRoute('gsb_covoit_homepage');
                 }
             }
         }
-
         return $this->render('GSBCovoitBundle:Covoit:login.html.twig', array(
             'form' => $form->createView(),
         ));
     }
-
     public function inscriptionAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
         $newSalarie = new Salarie();
         $salaries = $em->getRepository('GSBCovoitBundle:Salarie')->findAll();
-
         $form = $this->createFormBuilder($newSalarie)
             ->add('nom', TextType::class)
             ->add('prenom', TextType::class)
@@ -81,9 +55,7 @@ class LoginController extends Controller
             ->add('motDePasse', PasswordType::class)
             ->add('inscription', SubmitType::class, array('label' => "Inscription"))
             ->getForm();
-
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $newSalarie = $form->getData();
             $unique = true;
@@ -99,21 +71,15 @@ class LoginController extends Controller
                 {
                     $em->persist($newSalarie);
                     $em->flush();
-
                     // 1 - Idem, enregistrer les infos du $newSalarie en session
-
                     // 2 - Ajouter message flash :
                     //$this->addFlash('success','Félicitation, votre inscription est réussie !');
-
                     // 3 - L'afficher sur la page vers laquelle on se redirige
-
                     // 4 -[Si on a le temps] Envoyer un mail de confirmation d'inscription
-
                     return $this->redirectToRoute('gsb_covoit_homepage');
                 }
             }
         }
-
         return $this->render('GSBCovoitBundle:Covoit:login.html.twig', array(
             'form' => $form->createView(),
         ));
