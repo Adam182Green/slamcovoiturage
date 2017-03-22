1e9e5d79d2293530265ea4eddc7cdabe9f4f8a1f
Thibault Poujat <thibault.poujat@gmail.com>
2017-03-22 14:59:32 +0100

    Theme

--- src/GSB/CovoitBundle/Controller/LoginController.php
+++ src/GSB/CovoitBundle/Controller/LoginController.php
@@ -33,7 +33,6 @@ class LoginController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $login = $form->getData();
-            $login->setMotDePasse(sha1($login->getMotDePasse()));
             foreach($salaries as $salarie)
             {
                 if($salarie->getEmail() == $login->getEmail() && $salarie->getMotDePasse() == $login->getMotDePasse())
@@ -71,7 +70,6 @@ class LoginController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $newSalarie = $form->getData();
-            $newSalarie->setMotDePasse(sha1($newSalarie->getMotDePasse()));
             $unique = true;
             $nbSalaries = count($salaries);
             $index = 0;
